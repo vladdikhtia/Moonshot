a734fdc6f8e1cd9fbbeeb16ee899f8e190f36684
Vladyslav Dikhtiaruk <vlad.dikhtiaruk@gmail.com>
2024-03-22 18:44:04 +0100

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Vladyslav Dikhtiaruk on 22/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
