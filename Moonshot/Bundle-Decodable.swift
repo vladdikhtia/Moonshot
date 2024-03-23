879b92b411ef8ccd78ed059a177fd413b226b1b3
Vladyslav Dikhtiaruk <vlad.dikhtiaruk@gmail.com>
2024-03-23 17:50:46 +0100

    using generics to load any type of Codable data.2

--- Moonshot/Bundle-Decodable.swift
+++ Moonshot/Bundle-Decodable.swift
@@ -8,7 +8,24 @@
 import Foundation
 
 extension Bundle {
-    func decode(_ file: String) -> [String: Astronaut] {
+//    func decode(_ file: String) -> [String: Astronaut] {
+//        guard let url = self.url(forResource: file, withExtension: nil) else {
+//            fatalError("Failed to locate \(file) in bundle.")
+//        }
+//
+//        guard let data = try? Data(contentsOf: url) else {
+//            fatalError("Failed to load \(file) from bundle.")
+//        }
+//
+//        let decoder = JSONDecoder()
+//
+//        guard let loaded = try? decoder.decode([String: Astronaut].self, from: data) else {
+//            fatalError("Failed to decode \(file) from bundle.")
+//        }
+//
+//        return loaded
+//    }
+    func decode<T: Codable>(_ file: String) -> T {
         guard let url = self.url(forResource: file, withExtension: nil) else {
             fatalError("Failed to locate \(file) in bundle.")
         }
@@ -19,7 +36,7 @@ extension Bundle {
 
         let decoder = JSONDecoder()
 
-        guard let loaded = try? decoder.decode([String: Astronaut].self, from: data) else {
+        guard let loaded = try? decoder.decode(T.self, from: data) else {
             fatalError("Failed to decode \(file) from bundle.")
         }
 
