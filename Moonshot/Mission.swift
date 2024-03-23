eafd8d9c46c72fa58e8c6756cb630ffbd5775428
Vladyslav Dikhtiaruk <vlad.dikhtiaruk@gmail.com>
2024-03-23 18:26:28 +0100

    WIP: Mission View

--- Moonshot/Mission.swift
+++ Moonshot/Mission.swift
@@ -8,14 +8,28 @@
 import Foundation
 
 struct Mission: Codable, Identifiable {
+    
     struct CrewRole: Codable {
         let name: String
         let role: String
     }
-
-    let id: String
-    let launchDate: String?
+    
+    let id: Int
+    let launchDate: Date?
     let crew: [CrewRole]
     let description: String
+    
+    var displayName: String {
+        "Apollo \(id)"
+    }
+
+    var image: String {
+        "apollo\(id)"
+    }
+    
+    var formattedLaunchData: String {
+        launchDate?.formatted(date: .long, time: .omitted) ?? "N/A"
+    }
 }
+
 
